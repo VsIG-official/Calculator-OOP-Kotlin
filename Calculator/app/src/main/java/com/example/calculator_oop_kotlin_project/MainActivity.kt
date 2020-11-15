b2698e4cfff82efe2c7841bd7ced5d9a27a5f254
VsIG <Valentyn2202@gmail.com>
2020-11-15 16:54:15 +0200

    🎉: start the project

---
+++ Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.calculator_oop_kotlin_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
