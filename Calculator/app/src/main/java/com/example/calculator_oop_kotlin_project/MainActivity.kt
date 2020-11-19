5e64e54af17e4bbc0fe820d13b371a8fec329a71
VsIG <Valentyn2202@gmail.com>
2020-11-19 22:43:43 +0200

    ✨: add some functional for buttons

--- Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
@@ -4,17 +4,58 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Numbers
+        tvOne.setOnClickListener{ appendExpression("1",true)}
+        tvTwo.setOnClickListener{ appendExpression("2",true)}
+        tvThree.setOnClickListener{ appendExpression("3",true)}
+        tvFour.setOnClickListener{ appendExpression("4",true)}
+        tvFive.setOnClickListener{ appendExpression("5",true)}
+        tvSix.setOnClickListener{ appendExpression("6",true)}
+        tvSeven.setOnClickListener{ appendExpression("7",true)}
+        tvEight.setOnClickListener{ appendExpression("8",true)}
+        tvNine.setOnClickListener{ appendExpression("9",true)}
+        tvZero.setOnClickListener{ appendExpression("0",true)}
+        tvDot.setOnClickListener{ appendExpression(".",true)}
+
+        //Operators
+        tvPlus.setOnClickListener { appendExpression("+",false) }
+        tvMinus.setOnClickListener { appendExpression("-",false) }
+        tvMultiply.setOnClickListener { appendExpression("*",false) }
+        tvDivide.setOnClickListener { appendExpression("/",false) }
+        tvOpenPar.setOnClickListener { appendExpression("(",false) }
+        tvClosePar.setOnClickListener { appendExpression(")",false) }
+
+        tvClear.setOnClickListener {
+            tvExpression.text = ""
+            tvResult.text = ""
+        }
+
+        tvBack.setOnClickListener {
+            val string = tvExpression.text.toString()
+            if (string.isNotEmpty())
+            {
+                tvExpression.text=string.substring(0,string.length-1)
+            }
+            tvResult.text=""
+        }
     }
 
     fun appendExpression(string: String, canClear: Boolean){
-        if (canClear) {
+        if (canClear)
+        {
             tvResult.text = ""
             tvExpression.append(string)
-        }else {
+        }
+
+        else
+        {
             tvExpression.append(tvResult.text)
             tvExpression.append(string)
             tvResult.text=""
