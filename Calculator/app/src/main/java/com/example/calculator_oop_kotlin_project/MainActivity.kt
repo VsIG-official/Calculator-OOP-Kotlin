0bb7ff515d21eece21d06f738e748f8d8acb88f3
VsIG <Valentyn2202@gmail.com>
2020-11-19 15:44:57 +0200

    Update MainActivity.kt

--- Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.calculator_oop_kotlin_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun appendExpression(string: String, canClear: Boolean){
+        if (canClear) {
+            tvResult.text = ""
+            tvExpression.append(string)
+        }else {
+            tvExpression.append(tvResult.text)
+            tvExpression.append(string)
+            tvResult.text=""
+
+        }
     }
 }
