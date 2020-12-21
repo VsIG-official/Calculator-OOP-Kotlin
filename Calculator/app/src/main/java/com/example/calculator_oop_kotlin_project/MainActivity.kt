66f956569dee010f371c91035fe0c93faa3fb0db
VsIG <Valentyn2202@gmail.com>
2020-12-21 23:36:41 +0200

    ✨: add first functionality

--- Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator_oop_kotlin_project/MainActivity.kt
@@ -11,5 +11,53 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners()
+    {
+        options_btn.setOnClickListener {  }
+        clear_btn.setOnClickListener { clearAllText() }
+        plus_minus_btn.setOnClickListener {  }
+        one_divide_x_btn.setOnClickListener {  }
+        divide_btn.setOnClickListener {  }
+        multiply_btn.setOnClickListener {  }
+        minus_btn.setOnClickListener {  }
+        plus_minus_btn.setOnClickListener {  }
+        equals_btn.setOnClickListener {  }
+        back_btn.setOnClickListener { clearLastCharacter() }
+
+        nine_btn.setOnClickListener { appendText("9") }
+        eight_btn.setOnClickListener { appendText("8") }
+        seven_btn.setOnClickListener { appendText("7") }
+        six_btn.setOnClickListener { appendText("6") }
+        five_btn.setOnClickListener { appendText("5") }
+        four_btn.setOnClickListener { appendText("4") }
+        three_btn.setOnClickListener { appendText("3") }
+        two_btn.setOnClickListener { appendText("2") }
+        one_btn.setOnClickListener { appendText("1") }
+        zero_btn.setOnClickListener { appendText("0") }
+        dot_btn.setOnClickListener { appendText(".") }
+    }
+
+    private fun appendText(number: String)
+    {
+        second_numbers.append(number);
+    }
+
+    private fun clearAllText()
+    {
+        first_numbers.text=""
+        second_numbers.text=""
+    }
+
+    private fun clearLastCharacter()
+    {
+        second_numbers.text.dropLast(1)
+    }
+
+    private fun finishFirstNumber()
+    {
+
     }
 }
